5b66e4f356ae58c0e4d943214c907c681a536029
fulviocanducci <fulviocanducci@hotmail.com>
2020-12-17 14:58:04 -0300

    Second Commit

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Canducci\ZipCode\Providers\ZipCodeServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -226,7 +227,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'ZipCode' => Canducci\ZipCode\Facades\ZipCode::class,
+        'Address' => Canducci\ZipCode\Facades\Address::class,
     ],
 
 ];
